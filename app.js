0968ea749084497d2794ba73e7ed94f3d0c50ed1
LuisEstebanC <ing2.29capellan@gmail.com>
2024-02-14 23:56:20 -0400

    validating if is an int

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 function intToRomanConverter(number){
     if(typeof number !== 'number' || !Number.isInteger(number) ){
         throw new Error("It must be a number between 1 and 3999.")
-    }else{
-        console.log(number);
     }
+
+    const values =[
+        {value:1000, letter:'M'},
+        {value:900, letter:'CM'},
+        {value:500, letter:'D'},
+        {value:400, letter:'CD'},
+        {value:100, letter:'C'},
+        {value:90, letter:'XC'},
+        {value:50, letter:'L'},
+        {value:40, letter:'XL'},
+        {value:10, letter:'X'},
+        {value:9, letter:'IX'},
+        {value:5, letter:'V'},
+        {value:4, letter:'IV'},
+        {value:1, letter:'I'},
+
+    ]
 
 }
 
--- app.test.js
+++ app.test.js
@@ -4,3 +4,7 @@ const convert = require('./app')
 test('it should be a number', () => {
     expect(() => convert('sd')).toThrow("It must be a number between 1 and 3999.");
   });
+
+  test('it should be an int ', () => {
+    expect(() => convert(1.2)).toThrow("It must be a number between 1 and 3999.");
+  });
